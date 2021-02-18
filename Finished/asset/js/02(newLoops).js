74748b2ae3fa8f1b983b6d7bb30b4386af97c883
Solomon <se.solomn.kasahun@gmail.com>
2021-02-18 11:39:41 +0300

    answer for question two about loop

---
+++ Finished/asset/js/02(newLoops).js
@@ -0,0 +1,61 @@
+//DOM Load 
+document.addEventListener('DOMContentLoaded', () => {
+
+    forINLoop();
+
+    forOFLoop();
+    
+})
+
+
+
+function forINLoop() {
+
+    // A Student Object 
+    const student = { name: "Abebe", age: 21, year: "2dn year" }
+   
+    var x = document.getElementById('forInLoop')
+    //x.innerHTML = student.name
+    // 1. Create a for in loop that iterate over the student object
+    //for(key in student){}
+    for (key in student){
+        x.innerHTML +=
+        `<li class='list-group-item'>${key}:${student[key]}</li>`
+    }
+    
+    // 2. Inside the loop paint the UI [Use the Format Given Below]
+
+
+    //Use this Format 
+
+    // forInLoop.innerHTML +=
+    //     `
+    //     <li class="list-group-item" >${Key} : ${Value}</li>
+
+    // `;
+
+}
+
+
+function forOFLoop() {
+
+    // A Students Array
+    const students = [{ name: "Abebe", age: 21, year: "2dn year" }, { name: "Kebede", age: 22, year: "2dn year" }, { name: "Abebech", age: 23, year: "2dn year" }]
+    var x = document.getElementById('forOfLoop')
+    // 1. Create a for of loop that iterate over the students Array and displays the names of students 
+
+    for(value of students){
+        x.innerHTML += `<li class='list-group-item'>${value.name}</li>`
+    }
+    // 2. Inside the loop paint the UI [Use the Format Given Below]
+
+
+    //Use this Format 
+
+    // forOfLoop.innerHTML +=
+    //     `
+    //     <li class="list-group-item" >Name : Abebe</li>
+
+    // `;
+
+}

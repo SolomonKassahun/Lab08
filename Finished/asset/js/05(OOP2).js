5f1d81135aa6e4468b14aea0674e2d30996cbf3e
Solomon <se.solomn.kasahun@gmail.com>
2021-02-18 11:43:02 +0300

    answer for question OOP2

---
+++ Finished/asset/js/05(OOP2).js
@@ -0,0 +1,22 @@
+// 1. A Person Object for Base 
+const PersonObj = {
+
+    greeting() {
+        return `Hi , ${this.firstName} ${this.lastName}`;
+    }
+}
+
+//2. Create Theon Object using Person Template
+const Theon = {
+    firstName:"Theon",
+    lastName:"Greyjoy",
+    greeting() {
+        return `Hi , ${this.firstName} ${this.lastName}`
+    }
+}
+
+//3. Add firstName[Theon] and lastName[Greyjoy] 
+
+
+//4. Display the greeting on Theon Object [remove the string when you have the object]
+Obj_C2.innerHTML = Theon.greeting();

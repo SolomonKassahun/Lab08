dba5d1a52e7f2db93bb3d5ce335f52cb3a4c892a
Solomon <se.solomn.kasahun@gmail.com>
2021-02-18 11:42:32 +0300

    answer for question OOP1

---
+++ Finished/asset/js/05(OOP1).js
@@ -0,0 +1,22 @@
+// 1. A Person Constructor function 
+function Person(firstName, lastName) {
+
+    // 1. set the firstName and lastName using this keyword
+    this.firstName = firstName
+    this.lastName = lastName
+
+    //2. Create a greeting function returns [use this keyword to get the first and last name]
+    this.greating = function(){
+        return `Hi ${this.firstName} ${this.lastName}`
+    }
+    //  ==> Hi , firstName lastName
+
+
+
+} 
+
+//3. Create Jon Whose full name is ["Jon", "Snow"] Object using new keyword [new ConstructorFun()]
+const jon = new Person("jon", "snow")
+
+//4. Display the greeting on Jon Object [remove the string when you have the object]
+Obj_C1.innerHTML = jon.greating();

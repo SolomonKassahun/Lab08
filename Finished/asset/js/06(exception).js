84f422b0d660273934b4ab4a013c33a4620c369d
Solomon <se.solomn.kasahun@gmail.com>
2021-02-19 12:45:46 +0300

    answer for question about Exception

---
+++ Finished/asset/js/06(exception).js
@@ -0,0 +1,48 @@
+//Exception Handling using try catch 
+const fitsNo = document.querySelector("#fn");
+const secondNo = document.querySelector("#sn");
+const res = document.querySelector("#res");
+const btn_divide = document.querySelector("#btn_divide");
+
+btn_divide.addEventListener('click', divideNo);
+
+
+
+function divideNo() {
+
+    const no1 = parseInt(fitsNo.value)
+    const no2 = parseInt(secondNo.value)
+    if (fitsNo.value === '' || secondNo.value === '') {
+        alert("Enter Numbers");
+        return;
+    }
+    try{
+        const result = performDivision(no1, no2);
+
+        res.innerHTML = result;
+    }catch{
+        res.innerHTML = "error";
+    }
+    // 1. Define a try block 
+
+    // 2. put this two lines of code inside the try block 
+
+
+
+
+    //4.Catch the exception and display error message [remove the string when you have the error variable]
+    
+
+}
+
+
+
+
+//this function throw exception if denominator is zero
+function performDivision(x, y) {
+    if (y === 0) {
+        throw new Error("Can Not Divide by zero");
+    }
+
+    return x / y;
+}
